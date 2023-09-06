34439124bb1dab761fc9082ebd65ad3fd5a054c9
ReactedTitan880 <thomas.nardou@eduvaud.ch>
2023-09-06 08:31:21 +0200

    feat(parachute) add new size of the window

---
+++ exos/Parachute/Parachute/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parachute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+            Console.ReadLine();
+        }
+    }
+}
